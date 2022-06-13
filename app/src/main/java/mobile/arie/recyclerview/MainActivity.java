b9de7dccb041fcdbd352ec52b457ecd4f205d742
Riee <77129898+mongnay@users.noreply.github.com>
2022-06-13 14:30:22 +0700

    commit

---
+++ app/src/main/java/mobile/arie/recyclerview/MainActivity.java
@@ -0,0 +1,31 @@
+package mobile.arie.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.black_clover,R.drawable.boku_no_hero,R.drawable.jujutsu_kaisen,
+            R.drawable.fairy_tail,R.drawable.k_on};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.Manga);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
